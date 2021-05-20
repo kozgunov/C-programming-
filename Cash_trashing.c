c7df75e79f119c8244c06a0fc81de836d5503d49
kozgunov <78970376+kozgunov@users.noreply.github.com>
2021-05-20 11:20:59 +0300

    Update Cash_trashing.c

--- Cash_trashing.c
+++ Cash_trashing.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <time.h>
 
+#define N 10000
+float m[N][N];
+
+int main()
+{
+  float a = 0.0;
+
+  clock_t start, finish;
+  start = clock();
+
+  for(int n = 0; n < 100; ++n)
+    for(int i = 0; i < N; ++i)
+      for(int j = 0; j < N; ++j)
+        a += m[j][i];
+
+  finish = clock();
+
+  printf("%f: %f ms\n", a, ((double)(finish - start)) * 1000.0 / CLOCKS_PER_SEC);
+
+  return 0;
+}
+
+// 9216 Kb
